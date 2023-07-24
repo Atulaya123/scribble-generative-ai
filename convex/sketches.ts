cc64b2e8b301b127d1e7681b22f4ec6747924525
Atulaya123 <arai2_be20@thapar.edu>
2023-07-24 18:09:33 +0530

    Convex useQuery : ability to write and fetch sketches

--- convex/sketches.ts
+++ convex/sketches.ts
@@ -1,11 +1,17 @@
-import {  mutation } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
 export const saveSketch=mutation(({ db }, { prompt } :{ prompt : string}) => {
     console.log(prompt);
     db.insert("sketches",{
         prompt,
         createdAt: Date.now(),
     });
+
+
     return{
         message: "success",
     }
+});
+export const getSketches =query(async({db})=>{
+    const sketches = await db.query("sketches").collect();
+    return sketches;
 });
